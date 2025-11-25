ad3926ddf1c2baae8857c7ded7d451a3c5105269
waulkrie <aobare@gmail.com>
2025-11-25 17:41:36 +0000

    modules for descrete systems

--- flake.nix
+++ flake.nix
@@ -10,20 +10,40 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations.nixos-hypr-wk = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.anon = import ./home.nix;
-            backupFileExtension = "backup";
-          };
-        }
-      ];
+    nixosConfigurations = {
+      
+      nixos-hypr-wk = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/work/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.anon = import ./home.nix;
+              backupFileExtension = "backup";
+            };
+          }
+        ];
+      };
+
+      nixos-rig = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # lanzaboote.nixosModules.lanzaboote
+          ./hosts/home_desktop/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.anon = import ./home.nix;
+              backupFileExtension = "backup";
+            };
+          }
+        ];
+      };
     };
   };
 }
