68326475e6689aee4c58241e02f21b2e83914514
waulkrie <aobare@gmail.com>
2025-12-08 08:05:47 -0600

    ERROR: the option 'home' does not exist... no auto login work

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -1,17 +1,17 @@
 # hosts/work/configuration.nix
 # descrete settings per host
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
-    ../../configuration.nix  # Import shared config
+    ../../configuration.nix # Import shared config
     ./hardware-configuration.nix
   ];
 
   networking.hostName = "nixos-hypr-wk";
-
-  # Work-specific: autologin
-  services.getty.autologinUser = "anon";
 
   # Standard boot (no Secure Boot)
   boot.loader.systemd-boot.enable = true;
