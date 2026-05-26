8bd2fa7b6da40beccd3c30e1bb70edd5ac9e1c4a
waulkrie <aobare@gmail.com>
2026-05-26 14:57:43 -0500

    added nvidea legacy driver for quatro gfx card

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -16,4 +16,14 @@
   # Standard boot (no Secure Boot)
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # NVIDIA proprietary driver (Quadro M4000)
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+  };
+  hardware.graphics.enable = true;
 }
